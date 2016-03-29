30679ddbb4726cf926792ad73388cce6ff17c98c
Carlos Triana <trianag24@gmail.com>
2016-03-29 15:48:18 -0500

    Added RestClient

---
+++ nu3/app/src/main/java/utils/RestClient.java
@@ -0,0 +1,25 @@
+package utils;
+import com.loopj.android.http.*;
+
+/**
+ * Created by carlostriana on 3/29/16.
+ */
+
+public class RestClient {
+
+    private static final String BASE_URL = "https://api.twitter.com/1/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
