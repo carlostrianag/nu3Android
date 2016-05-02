75e409bd88046f6550314a601f8ed1406bbe6eec
Andres Castañeda Borre <borref22@gmail.com>
2016-05-01 20:16:52 -0500

    add paypal single payment

---
+++ nu3/app/src/main/java/com/nu3/views/DonateFragment.java
@@ -0,0 +1,87 @@
+package com.nu3.views;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nu3.R;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+
+import java.math.BigDecimal;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DonateFragment extends Fragment {
+
+
+    private static final String TAG = "paymentExample";
+    /**
+     * - Set to PayPalConfiguration.ENVIRONMENT_PRODUCTION to move real money.
+     *
+     * - Set to PayPalConfiguration.ENVIRONMENT_SANDBOX to use your test credentials
+     * from https://developer.paypal.com
+     *
+     * - Set to PayPalConfiguration.ENVIRONMENT_NO_NETWORK to kick the tires
+     * without communicating to PayPal's servers.
+     */
+    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_NO_NETWORK;
+
+    // note that these credentials will differ between live & sandbox environments.
+    private static final String CONFIG_CLIENT_ID = "credentials from developer.paypal.com";
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+    private static final int REQUEST_CODE_PROFILE_SHARING = 3;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(CONFIG_ENVIRONMENT)
+            .clientId(CONFIG_CLIENT_ID)
+                    // The following are only used in PayPalFuturePaymentActivity.
+            .merchantName("Fundacion Nu3")
+            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
+
+    PayPalPayment thingToBuy;
+
+
+    public DonateFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_donate, container, false);
+        Intent intent = new Intent(getContext(), PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        getActivity().startService(intent);
+
+        rootView.findViewById(R.id.single_payment_paypal_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thingToBuy = new PayPalPayment(new BigDecimal("10"), "USD",
+                        "HeadSet", PayPalPayment.PAYMENT_INTENT_SALE);
+                Intent intent = new Intent(getContext(),
+                        PaymentActivity.class);
+                intent.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
+                startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+
+            }
+        });
+
+        return rootView;
+    }
+
+}
